8920b89221cff86a1b927409b35a9d55392baad5
karthik reddy <karthik.reddy@riverbed.com>
2016-08-08 14:29:44 -0700

    diable sunburn

--- init.el
+++ init.el
@@ -1,6 +1,8 @@
 (require 'package)
 (add-to-list 'package-archives
-	     '("melpa" . "http://melpa.milkbox.net/packages/") t)
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives
+             '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 (package-initialize)
 
 (setq gc-cons-threshold 100000000)
@@ -185,7 +187,7 @@
 
 
 ;;zenburn theme
-(load-theme 'zenburn t)
+;; (load-theme 'zenburn t)
 
 (setq org-agenda-files (list "~/Documents/work.org"
                              "~/Documents/school.org"
@@ -195,10 +197,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(safe-local-variable-values (quote ((c-indent-level . 8)))))
+ '(bmkp-last-as-first-bookmark-file "/home/kareddy/.emacs.d/bookmarks")
+ '(safe-local-variable-values (quote ((c-indent-level . 8))))
+ '(show-paren-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(global-set-key (kbd "C-x g") 'magit-status)
+(require 'bookmark+)
