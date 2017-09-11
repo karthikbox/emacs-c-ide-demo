7440d2d6efdc7254a54e4c04c73ac68b80684198
karthik reddy <karthik.reddy@riverbed.com>
2017-09-11 12:03:14 -0700

    Cleanup init.el
    Refer
    http://y.tsutsumi.io/emacs-from-scratch-part-3-extending-emacs-with-elisp.html

---
+++ my-loadpackages.el
@@ -0,0 +1,164 @@
+; loading package
+(load "~/.emacs.d/my-packages.el")
+
+(global-linum-mode 1)
+
+(add-to-list 'load-path "~/.emacs.d/custom")
+;; XXX change this
+(add-to-list 'load-path "~/doxymacs-1.8.0/lisp")
+
+(require 'setup-helm)
+(require 'setup-helm-gtags)
+(require 'setup-ggtags)
+(require 'setup-cedet)
+(require 'setup-editing)
+
+(windmove-default-keybindings)
+
+;; company
+(require 'company)
+(add-hook 'after-init-hook 'global-company-mode)
+(delete 'company-semantic company-backends)
+(define-key c-mode-map  [(tab)] 'company-complete)
+(define-key c++-mode-map  [(tab)] 'company-complete)
+;; (define-key c-mode-map  [(control tab)] 'company-complete)
+;; (define-key c++-mode-map  [(control tab)] 'company-complete)
+
+;; company-c-headers
+(add-to-list 'company-backends 'company-c-headers)
+
+;; hs-minor-mode for folding source code
+(add-hook 'c-mode-common-hook 'hs-minor-mode)
+
+
+;; SF C style
+(defconst my-c-style
+  '((c-basic-offset             . 4)
+    (c-hanging-comment-ender-p  . nil)
+    (c-offsets-alist
+     . ((substatement-open      . 0)            ;don't indent braces!
+        (inline-open            . 0)            ;don't indent braces, dammit!
+        (label                  . -1000)        ;flush labels left
+        (innamespace            . 0)
+        (statement-cont         . c-lineup-math)))))
+
+(c-add-style "my-c" my-c-style)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(bmkp-last-as-first-bookmark-file "~/.emacs.d/bookmarks")
+ '(c-default-style (quote ((other . "my-c"))))
+ '(fill-column 80)
+ '(indent-tabs-mode nil)
+ '(magit-pull-arguments nil)
+ '(magit-push-arguments (quote ("--force")))
+ '(safe-local-variable-values (quote ((c-indent-level . 8))))
+ '(show-paren-mode t))
+
+;; Available C style:
+;; “gnu”: The default style for GNU projects
+;; “k&r”: What Kernighan and Ritchie, the authors of C used in their book
+;; “bsd”: What BSD developers use, aka “Allman style” after Eric Allman.
+;; “whitesmith”: Popularized by the examples that came with Whitesmiths C, an early commercial C compiler.
+;; “stroustrup”: What Stroustrup, the author of C++ used in his book
+;; “ellemtel”: Popular C++ coding standards as defined by “Programming in C++, Rules and Recommendations,” Erik Nyquist and Mats Henricson, Ellemtel
+;; “linux”: What the Linux developers use for kernel development
+;; “python”: What Python developers use for extension modules
+;; “java”: The default style for java-mode (see below)
+;; “user”: When you want to define your own style
+;; (setq
+;;  c-default-style "linux" ;; set style to "linux"
+;;  )
+
+(global-set-key (kbd "RET") 'newline-and-indent)  ; automatically indent when press RET
+
+;; activate whitespace-mode to view all whitespace characters
+(global-set-key (kbd "C-c w") 'whitespace-mode)
+
+;; show unncessary whitespace that can mess up your diff
+(add-hook 'prog-mode-hook (lambda () (interactive) (setq show-trailing-whitespace 1)))
+
+;; use space to indent by default
+(setq-default indent-tabs-mode nil)
+
+;; set appearance of a tab that is represented by 4 spaces
+(setq-default tab-width 4)
+
+;; setup GDB
+(setq
+ ;; use gdb-many-windows by default
+ gdb-many-windows t
+
+ ;; Non-nil means display source file containing the main routine at startup
+ gdb-show-main t
+ )
+
+;; Package: clean-aindent-mode
+(require 'clean-aindent-mode)
+(add-hook 'prog-mode-hook 'clean-aindent-mode)
+
+;; Package: dtrt-indent
+(require 'dtrt-indent)
+(dtrt-indent-mode 1)
+
+;; Package: ws-butler
+(require 'ws-butler)
+(add-hook 'prog-mode-hook 'ws-butler-mode)
+
+;; Package: yasnippet
+(require 'yasnippet)
+(yas-global-mode 1)
+
+;; Package: smartparens
+(require 'smartparens-config)
+(setq sp-base-key-bindings 'paredit)
+(setq sp-autoskip-closing-pair 'always)
+(setq sp-hybrid-kill-entire-symbol nil)
+(sp-use-paredit-bindings)
+
+(show-smartparens-global-mode +1)
+(smartparens-global-mode 1)
+
+;; Package: projejctile
+(require 'projectile)
+(projectile-global-mode)
+(setq projectile-enable-caching t)
+
+(require 'helm-projectile)
+(helm-projectile-on)
+(setq projectile-completion-system 'helm)
+(setq projectile-indexing-method 'alien)
+
+;; Package zygospore
+(global-set-key (kbd "C-x 1") 'zygospore-toggle-delete-other-windows)
+
+
+;;zenburn theme
+;; (load-theme 'zenburn t)
+
+(setq org-agenda-files (list "~/Documents/work.org"
+                             "~/Documents/school.org"
+                             "~/Documents/personal.org"))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(global-set-key (kbd "C-x g") 'magit-status)
+(require 'bookmark+)
+(require 'xcscope)
+(cscope-setup)
+
+(require 'whitespace)
+(setq whitespace-style '(face empty tabs lines-tail trailing))
+ (global-whitespace-mode t)
+
+(global-set-key (kbd "M-1") 'isearch-forward-symbol-at-point)
+
+(require 'doxymacs)
+(add-hook 'c-mode-common-hook'doxymacs-mode)
---
+++ my-packages.el
@@ -0,0 +1,51 @@
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-initialize)
+
+(defvar required-packages
+  '(
+    anzu
+    company
+    duplicate-thing
+    ggtags
+    helm
+    helm-gtags
+    helm-projectile
+    helm-swoop
+    ;; function-args
+    clean-aindent-mode
+    comment-dwim-2
+    dtrt-indent
+    ws-butler
+    iedit
+    yasnippet
+    magit
+    smartparens
+    projectile
+    volatile-highlights
+    undo-tree
+    bookmark+
+    zenburn-theme
+    xcscope
+    zygospore
+    ) "a list of packages to ensure are installed at launch.")
+
+;; method to check if all packages are installed
+(defun packages-installed-p ()
+  (dolist (p required-packages)
+    (when (not (package-installed-p p)) do (return nil)
+        finally (return t))))
+
+;; if not all packages are installed, check one by one and install the missing ones.
+(unless (packages-installed-p)
+  ;; check for new packages (package versions)
+  (message "%s" "Emacs is now refreshing its package database...")
+  (package-refresh-contents)
+  (message "%s" " done.")
+  ;; install the missing packages
+  (dolist (p required-packages)
+    (when (not (package-installed-p p))
+      (package-install p))))
